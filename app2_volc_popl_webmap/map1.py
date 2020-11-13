daea7698f7e513566c032598729f1f2b147a0b16
Amol Gawai <5114143+amolgawai@users.noreply.github.com>
2020-11-13 19:58:44 +0100

    added colors to marker and changed marker shape to circlemarker

--- app2_volc_popl_webmap/map1.py
+++ app2_volc_popl_webmap/map1.py
@@ -16,6 +16,18 @@ Volcano name:<br>
 Height: %s m
 """
 
+def color_from_elevation(elevation):
+    """Returns a color based on elevation
+    Keyword Arguments:
+    elevation -- the elevation value
+    """
+    if elevation < 1000:
+        return 'green'
+    if 1000 <= elevation < 3000:
+        return 'orange'
+
+    return 'red'
+
 
 my_map = folium.Map(location=[38.58, -99.09],
                     zoom_start=6,
@@ -25,9 +37,15 @@ fg = folium.FeatureGroup(name="Volcanos")
 for lt, ln, el, name in zip(lat, lon, elev, name):
     popup_iframe = folium.IFrame(html=POPUP_HTML % (name, name, el),
                                  width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln],
-                               popup=folium.Popup(popup_iframe),
-                               icon=folium.Icon(color="green")))
+    fg.add_child(folium.CircleMarker(location=[lt, ln],
+									 color='grey',
+									 fill=True,
+									 fill_color=color_from_elevation(el),
+									 fill_opacity=0.7,
+                                	 popup=folium.Popup(popup_iframe)))
+    # fg.add_child(folium.Marker(location=[lt, ln],
+    #                            popup=folium.Popup(popup_iframe),
+    #                            icon=folium.Icon(color=color_from_elevation(el))))
 
 my_map.add_child(fg)
 my_map.save("Map1.html")
