798a63ab09c2ab6a43b28b2458c6d0e5735cbec3
Amol <amol@doesnot.exist>
2020-11-26 17:13:48 +0000

    added sign up screen

--- app5_mobile_app/app_main.py
+++ app5_mobile_app/app_main.py
@@ -5,15 +5,30 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-Builder.load_file('AppDesign.kv')
-
-
-class LoginScreen(Screen):
-    pass
-
 
 class RootWidget(ScreenManager):
     pass
+
+
+class LoginScreen(Screen):
+    def sign_up(self):
+        """Sign up button press callback
+        """
+        self.manager.current = "sign_up_screen"
+
+
+class SignUpScreen(Screen):
+    def new_account(self, uname, pwd):
+        """ Create a new account
+
+        Parameters
+        ----------
+        uname : the username
+
+        pwd : the password
+
+        """
+        print(uname, pwd)
 
 
 class MainApp(App):
@@ -22,4 +37,5 @@ class MainApp(App):
 
 
 if __name__ == "__main__":
+    Builder.load_file('AppDesign.kv')
     MainApp().run()
