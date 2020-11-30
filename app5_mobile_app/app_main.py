fe795dc8dd11643eb9d27c9f38663f5e41c56794
Amol Gawai <5114143+amolgawai@users.noreply.github.com>
2020-11-30 15:15:17 +0100

    completed the first version of app
    As per the course instructions, the app is now function complete...

--- app5_mobile_app/app_main.py
+++ app5_mobile_app/app_main.py
@@ -4,6 +4,9 @@
 from datetime import datetime
 import os
 import json
+import glob
+from pathlib import Path
+import random
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -11,11 +14,27 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 
 USER_DB = "user_db.json"
 
+def get_users():
+    users = dict()
+    with open(USER_DB) as db_file:
+        db_file.seek(0)
+        if db_file.read(1):
+            db_file.seek(0)
+            users = json.load(db_file)
+    return users
+
+
 class RootWidget(ScreenManager):
     pass
 
 
 class LoginScreen(Screen):
+    def log_in(self, uname, pwd):
+        users = get_users()
+        if uname in users and users[uname]['password'] == pwd:
+            self.manager.current = "app_main_screen"
+        else:
+            self.ids.login_fail.text = "Wrong username or password"
     def sign_up(self):
         """Sign up button press callback
         """
@@ -33,17 +52,38 @@ class SignUpScreen(Screen):
         pwd : the password
 
         """
-        users = dict()
-        with open(USER_DB) as db_file:
-            db_file.seek(0)
-            if db_file.read(1):
-                users = json.load(db_file)
-
+        users = get_users()
         users[uname] = {'username': uname,
                         'password': pwd,
                         'created': datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
         with open(USER_DB, 'w') as db_file:
             json.dump(users, db_file)
+        self.manager.current = "sign_up_screen_success"
+
+
+
+class SignUpScreenSuccess(Screen):
+    def show_login(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
+
+
+
+class AppMainScreen(Screen):
+    def enlighten(self, feeling):
+        feeling = feeling.lower()
+        files = glob.glob("quotes/*txt")
+        feelings_available = [Path(filename).stem for filename in files]
+        if feeling in feelings_available:
+            with open(f"quotes/{feeling}.txt") as quote_file:
+                quotes = quote_file.readlines()
+            self.ids.quote.text = random.choice(quotes)
+        else:
+            self.ids.quote.text = "Try another feeling"
+    def log_out(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
+
 
 class MainApp(App):
     def build(self):
